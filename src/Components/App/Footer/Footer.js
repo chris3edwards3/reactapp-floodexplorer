d741738b351fff61ffc014993ac1633eff170c42
chris <chris3edwards3@gmail.com>
2020-04-08 08:23:53 -0600

    Styling the Modal for the Icon Buttons.

--- src/Components/App/Footer/Footer.js
+++ src/Components/App/Footer/Footer.js
@@ -2,19 +2,31 @@
 
 import React from 'react';
 import './Footer.css';
-import questionIcon from "./icons/question.PNG";
-import infoIcon from "./icons/info.PNG";
-import legendIcon from "./icons/legend.PNG";
+import MyIcon from "./MyIcon/MyIcon";
+// import questionIcon from "./icons/question.PNG";
+// import infoIcon from "./icons/info.PNG";
+// import legendIcon from "./icons/legend.PNG";
 
 function Footer() {
     return (
         <div id="Footer">
-            <div><img src={questionIcon} className="icon" alt="question"/></div>
-            <div><img src={infoIcon} className="icon" alt="info"/></div>
-            <div><img src={legendIcon} className="icon" alt="legend"/></div>
+            <MyIcon id="question" modalId="questionModal" />
+            <MyIcon id="info" modalId="infoModal" />
+            <MyIcon id="legend" modalId="legendModal" />
             <div id="copyright">&copy; 2020 | Chris Edwards | Built for IS-542 at BYU</div>
         </div>
     )
 }
+
+// function Footer() {
+//     return (
+//         <div id="Footer">
+//             <img src={questionIcon} className="icon" alt="question" />
+//             <img src={infoIcon} className="icon" alt="info"/>
+//             <img src={legendIcon} className="icon" alt="legend"/>
+//             <div id="copyright">&copy; 2020 | Chris Edwards | Built for IS-542 at BYU</div>
+//         </div>
+//     )
+// }
 
 export default Footer;
---
+++ src/Components/App/Footer/MyIcon/MyIcon.js
@@ -0,0 +1,61 @@
+// TODO: Style the Modal Popup
+
+import React from 'react';
+import "./MyIcon.css";
+import questionIcon from "./myIconFiles/question.PNG";
+import infoIcon from "./myIconFiles/info.PNG";
+import legendIcon from "./myIconFiles/legend.PNG";
+
+let iconSource = {
+    "info": infoIcon,
+    "legend": legendIcon,
+    "question": questionIcon,
+};
+
+let messageText = {
+    "info":
+`this is 
+the 
+information button`,
+    "legend": "this is the legend button",
+    "question": "this is the question button",
+};
+
+class MyIcon extends React.Component {
+    constructor() {
+        super();
+        this.handleIconClick = this.handleIconClick.bind(this);
+        this.handleSpanClick = this.handleSpanClick.bind(this);
+    }
+
+    handleIconClick() {
+        let modal = document.getElementById(this.props.modalId);
+        modal.style.display = "block";
+    }
+
+    handleSpanClick() {
+        let modal = document.getElementById(this.props.modalId);
+        modal.style.display = "none";
+    }
+
+    render() {
+        let modalId = this.props.modalId;
+
+        return (
+            <div>
+                <img className="icon" src={iconSource[this.props.id]} alt={this.props.id} onClick={this.handleIconClick}/>
+                <div id={modalId} className="modal">
+                    {/*Modal Content*/}
+                    <div className="modal-content">
+                        <span className="close" onClick={this.handleSpanClick}>&times;</span>
+                        <p>{messageText[this.props.id]}</p>
+                    </div>
+
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default MyIcon;
