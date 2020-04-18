8a4394f812c9ab212952dd5777275cdcf3ff3012
chris <chris3edwards3@gmail.com>
2020-04-18 15:42:38 -0600

    make page more responsive, focussing on cell phone screens.

--- src/Components/App/Footer/Footer.js
+++ src/Components/App/Footer/Footer.js
@@ -5,10 +5,12 @@ import MyIcon from "./MyIcon/MyIcon";
 function Footer() {
     return (
         <div id="Footer">
-            <MyIcon id="question" modalId="questionModal" />
-            <MyIcon id="flood" modalId="floodModal" />
-            <MyIcon id="additional" modalId="additionalModal" />
-            <MyIcon id="baseMap" modalId="baseMapModal" />
+            <div id="icons">
+                <MyIcon id="question" modalId="questionModal" />
+                <MyIcon id="flood" modalId="floodModal" />
+                <MyIcon id="additional" modalId="additionalModal" />
+                <MyIcon id="baseMap" modalId="baseMapModal" />
+            </div>
             <div id="copyright">&copy; 2020 | Chris Edwards | Built for IS-542 at BYU</div>
         </div>
     )
